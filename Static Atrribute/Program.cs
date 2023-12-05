10b7ac2229df1d5de57f96afdb70e1b185acbf98
A253560 <A253560@LSBSA-CCTCSV3>
2023-12-05 18:04:20 +0200

    Add project files.

---
+++ Static Atrribute/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Atrribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Songs holiday = new songs("Holiday", "Green Day");
+            Songs kashmir = new songs("Holiday", "Green Day");
+
+            Console.WriteLine(Songs.songCount);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Static Atrribute/Songs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Atrribute
+{
+    internal class Songs
+    {
+        public string title;
+        public string artist;
+        public int duration;
+
+        public static int songCount = 0;
+        public Songs(string aTitle, string aArtist, int aDuration)
+        {
+            this.title = aTitle;
+            this.artist = aArtist;
+            this.duration = aDuration;  
+            songCount++;
+        }
+    }
+}
